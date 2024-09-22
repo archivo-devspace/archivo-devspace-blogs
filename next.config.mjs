e522e03e71cae9994f3a09871e1ef99d7f979f41
HtetThuYaAungg <htetthuya.htya@gmail.com>
2024-09-22 22:45:46 +0630

    [ENHANCEMENT] add base path at next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath:
+    "/archivo-devspace-knowledge-sharing/blogs/detail/better-software-programmer-series",
 };
 
 export default nextConfig;
